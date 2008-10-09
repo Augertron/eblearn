3061ffbc701514f215efa0cf27fc0ffecf33afac
cpoulet <cpoulet@082c7e19-22b6-4319-94e1-375644db782b>
2008-10-09 19:07:02 +0000

    Added the facenet project into the "demos", with the minimal data to use the detection part of it on a sample. The training data is not included as it is way too big (500Mo...)

---
+++ trunk/eblearn/demos/facenet/CMakeFiles/facenet.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+"CMakeFiles/facenet.dir/src/facenet.o"
+"CMakeFiles/facenet.dir/src/main.o"
+"../../../bin/facenet.pdb"
+"../../../bin/facenet"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/facenet.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
---
+++ trunk/eblearn/gui/CMakeFiles/eblearn_gui.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+FILE(REMOVE_RECURSE
+"CMakeFiles/eblearn_gui.dir/src/moc_ebm_gui.o"
+"CMakeFiles/eblearn_gui.dir/src/ebm_gui.o"
+"../../bin/libeblearn_gui.pdb"
+"../../bin/libeblearn_gui.a"
+"../../bin/libeblearn_gui.so"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/eblearn_gui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
