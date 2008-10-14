1a6675c4253a51cae38f8c3a5a61ea3354a93a85
cpoulet <cpoulet@082c7e19-22b6-4319-94e1-375644db782b>
2008-10-14 23:06:36 +0000

    added a test for save/load long idx

--- trunk/eblearn/tester/src/IdxIOTest.cpp
+++ trunk/eblearn/tester/src/IdxIOTest.cpp
@@ -8,15 +8,15 @@ void IdxIOTest::setUp() {
 
 void IdxIOTest::tearDown() {
 }
-	
+
 template<class T> void test_save_load_matrix() {
 	Idx<T> m(9, 9);
 	Idx<T> l(1, 1);
 	string fname= "/tmp/libeblearn_tester_matrix.mat";
-	
+
 	// initialize values
 	double v = 0.1;
-	{ idx_aloop1(i, m, T) { 
+	{ idx_aloop1(i, m, T) {
 		*i = (T) v;
 		v++;
 	  }
@@ -27,7 +27,7 @@ template<class T> void test_save_load_matrix() {
 	//l.printElems(); cout << endl;
 	// test values
 	v = 0.1;
-	{ idx_aloop1(i, l, T) { 
+	{ idx_aloop1(i, l, T) {
 		CPPUNIT_ASSERT_EQUAL((T)v, *i);
 		v++;
 	  }
@@ -48,4 +48,27 @@ void IdxIOTest::test_save_load_matrix_float() {
 
 void IdxIOTest::test_save_load_matrix_double() {
 	test_save_load_matrix<double>();
+}
+
+void IdxIOTest::test_save_load_matrix_long() {
+	Idx<intg> test(3,3);
+	test.set(2147483647, 0, 0);
+	test.set(2147483646, 0, 1);
+	test.set(2147483645, 0, 2);
+	test.set(4, 1, 0);
+	test.set(5, 1, 1);
+	test.set(6, 1, 2);
+	test.set(-2147483647, 2, 0);
+	test.set(-2147483646, 2, 1);
+	test.set(-2147483645, 2, 2);
+
+	CPPUNIT_ASSERT(save_matrix(test, "/tmp/libeblearn_tester_matrix.mat") == true);
+
+	Idx<long> m(1,1);
+	CPPUNIT_ASSERT(load_matrix(m, "/tmp/libeblearn_tester_matrix.mat") == true);
+
+	{ idx_aloop2(i, test, long, j, m, long) {
+		CPPUNIT_ASSERT_EQUAL(*j, *i);
+	  }
+	}
 }
