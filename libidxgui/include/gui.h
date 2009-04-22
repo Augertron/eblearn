e70820255fd0b319651be6e04d39c6a90a78480a
sermanet <sermanet@082c7e19-22b6-4319-94e1-375644db782b>
2009-04-22 18:39:57 +0000

    adding gui.*

---
+++ libidxgui/include/gui.h
@@ -0,0 +1,148 @@
+/***************************************************************************
+ *   Copyright (C) 2009 by Pierre Sermanet *
+ *   pierre.sermanet@gmail.com *
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Redistribution under a license not approved by the Open Source
+ *       Initiative (http://www.opensource.org) must display the
+ *       following acknowledgement in all advertising material:
+ *        This product includes software developed at the Courant
+ *        Institute of Mathematical Sciences (http://cims.nyu.edu).
+ *     * The names of the authors may not be used to endorse or promote products
+ *       derived from this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESS OR IMPLIED
+ * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL ThE AUTHORS BE LIABLE FOR ANY
+ * DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ ***************************************************************************/
+
+#ifndef GUI_H_
+#define GUI_H_
+
+#include <ostream>
+#include "libidx.h"
+#include "gui_thread.h"
+
+using namespace std;
+
+namespace ebl {
+  
+  //! creates a new window.
+  unsigned int new_window(const char *wname = NULL, unsigned int h = 0,
+			  unsigned int w = 0);
+
+  //! selects window wid.
+  void select_window(unsigned int wid);
+
+  //! do not update display until enable_updates is called again.
+  //! this should make the display faster.
+  void disable_window_updates();
+
+  //! allow display updates and display if it was previously off after a
+  //! call to disable_updates().
+  void enable_window_updates();
+
+  //! draw_matrix displays your idx2 or the first layer of your idx3 in
+  //! grayscale on the whiteboard. This function does a copy of your idx and
+  //! won't change in in any way !
+  //! @param idx and @param type are, like before, used to templatize the
+  //! function
+  //! @param x and @param y are the coordinates of the top-left corner of
+  //! your picture on the whiteboard
+  //! @param minv and @param maxv are the min and max values to set colors.
+  //! If left to zero, the min of your idx will be set to 0 and the max will
+  //! be 255
+  //! @param zoomw and @param zoomh are the zoom factors in width and height
+  template<class T>
+    void draw_matrix(idx<T> &im, unsigned int h0 = 0, unsigned int w0 = 0,
+		     double zoomh = 1.0, double zoomw = 1.0,
+		     T minv = 0, T maxv = 0);
+    
+  //! same as draw_matrix but draws a frame of color (r,g,b) around it.
+  template<class T>
+    void draw_matrix_frame(idx<T> &im, ubyte r, ubyte g, ubyte b,
+			   unsigned int h0 = 0, unsigned int w0 = 0,
+			   double zoomh = 1.0, double zoomw = 1.0,
+			   T minv = 0, T maxv = 0);
+
+  //! same a draw_matrix but overlays the string <str> in the top left corner.
+  template<class T>
+    void draw_matrix(idx<T> &im, const char *str,
+		     unsigned int h0 = 0, unsigned int w0 = 0,
+		     double zoomh = 1.0, double zoomw = 1.0,
+		     T minv = 0, T maxv = 0);
+
+  //! closes all windows.
+  void quit_gui();
+  
+  //! clears the window.
+  void clear_window();
+
+  //! draws an arrow from (h1, w1) to (h2, w2).
+  void draw_arrow(int h1, int w1, int h2, int w2);
+
+  //! do not show windows, instead save them in png files in current dir.
+  void set_gui_silent();
+  //! do not show windows, instead save them in png files in current dir.
+  void set_gui_silent(const std::string *filename);
+  //! do not show windows, instead save them in png files in current dir.
+  void set_gui_silent(const char *filename);
+  
+  //! draws text on the current window.
+  //! you can also use the << operator instead of this function to add text 
+  //! to the gui. for example: gui << "text" << endl;
+  void draw_text(std::string *s);
+  
+  //! draws text on the current window at origin (h0, w0).
+  //! you can also use the << operator instead of this function to add text 
+  //! to the gui. for example: gui << at(h0, w0) << "text" << endl;
+  void draw_text(std::string *s, unsigned int h0, unsigned int w0);
+
+  //! sets the origin of further calls to draw_text or gui << "text".
+  //! you can also use the at() function instead of this one.
+  //! for example: gui << at(42, 0) << "text";
+  void set_text_origin(unsigned int h0, unsigned int w0);
+
+  //! sets the text color for further calls to draw_text or gui << "text".
+  //! you can also use the set_colors() function to set
+  //! text and background colors and transparency.
+  //! for example: gui << set_colors(255, 255, 255, 255, 0, 0, 0, 127);
+  //! this sets the text color to fully opaque white on a semi-transparent
+  //! black background.
+  void set_text_colors(unsigned char fg_r, unsigned char fg_g,
+		       unsigned char fg_b, unsigned char fg_a,
+		       unsigned char bg_r, unsigned char bg_g,
+		       unsigned char bg_b, unsigned char bg_a);
+  //! see unsigned char version.
+  void set_text_colors(int fg_r, int fg_g, int fg_b, int fg_a,
+		       int bg_r, int bg_g, int bg_b, int bg_a);
+
+  //! set the << operator to output text on both std::cout and the current
+  //! window.
+  //! you can also use the cout_and_gui() function.
+  //! for example: gui << cout_and_gui() << "text";
+  void set_window_cout_and_gui();
+
+  //! set the << operator to output text only to the current window.
+  //! you can also use the gui_only() function and not to std::cout.
+  //! for example: gui << gui_only() << "text";
+  void set_window_gui_only();
+
+} // namespace ebl
+
+#include "gui.hpp"
+
+#endif /* GUI_H_ */
