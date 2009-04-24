6dedd53e91d2316be26e62890b1fb7140c07c995
sermanet <sermanet@082c7e19-22b6-4319-94e1-375644db782b>
2009-04-24 23:32:15 +0000

    codec.

--- libeblearn/src/ebl_codec.cpp
+++ libeblearn/src/ebl_codec.cpp
@@ -45,13 +45,42 @@ namespace ebl {
   }
 
   void codec::fprop(state_idx &in1, state_idx &in2, state_idx &energy) {
-    encoder.fprop(in1, enc_out);
-    decoder.fprop(enc_out, dec_out);
-    z_cost.fprop(enc_out, z_energy); // sparsity cost
-    enc_cost.fprop(enc_out, z, enc_energy); // encoder energy
-    dec_cost.fprop(dec_out, in2, dec_energy); // decoder energy
+    // initialize z with a simple one-pass fprop through whole machine
+    fprop_one_pass(in1, in2, energy);
+    // now do gradient descent to find optimal code z
+    bprop_dec(in1, in2, energy);
   }
   
+  // simple one-pass forward propagation
+  void codec::fprop_one_pass(state_idx &in1, state_idx &in2, 
+			     state_idx &energy) {
+    encoder.fprop(in1, enc_out);
+    // let the enc-cost produce its best guess
+    // for what the code should be. If the cost
+    // is an l2-distance, this will simply 
+    // copy :enc-out:x into :z:x
+    // There is no need to do an fprop of
+    // the encoder afterward, because infer2 does it.
+    enc_cost.infer2_copy(enc_out, z, enc_energy);
+    // compute cost penalty
+    z_cost.fprop(z, z_energy);
+    // fprop through decoder
+    decoder.fprop(z, dec_out);
+    // fprop through decoder cost.
+    dec_cost.fprop(dec_out, in2, dec_energy);
+    // add up energy terms
+    energy.x.clear();
+    idx_dotcacc(enc_energy.x, weight_energy_enc, energy.x);
+    idx_dotcacc(dec_energy.x, weight_energy_dec, energy.x);
+    idx_dotcacc(z_energy.x, weight_energy_z, energy.x);
+  }
+  
+  // multiple-pass bprop on the decoder only to find the optimal code z
+  void codec::bprop_dec(state_idx &in1, state_idx &in2, 
+			state_idx &energy) {
+    z.dx.clear();
+  }
+
   void codec::bprop(state_idx &in1, state_idx &in2, state_idx &energy) {
   }
 
