357648d063e2bf796e6dee4ebfa5e2906d0f3c2a
sermanet <sermanet@082c7e19-22b6-4319-94e1-375644db782b>
2009-06-03 22:01:36 +0000

    fixing meta_trainer.

--- libeblearntools/src/meta_trainer.cpp
+++ libeblearntools/src/meta_trainer.cpp
@@ -33,6 +33,7 @@
 #include <fstream>
 #include <iostream>
 #include <stdio.h>
+#include <map>
 
 #ifdef __BOOST__
 #include "boost/filesystem.hpp"
@@ -45,6 +46,9 @@ using namespace boost;
 
 using namespace std;
 using namespace ebl;
+
+typedef map<string, map<string, vector<string>, less<string> >, less<string> >
+plots_t;
 
 // parse command line input
 bool parse_args(int argc, char **argv) {
@@ -100,110 +104,27 @@ void print_usage() {
   cout << "  -deformations <integer>" << endl;
 }
 
-bool append_plotter(const string &name, const string &vname, int col) {
-  string fname = vname;
-  fname += ".p";
-  ifstream in(fname.c_str());
-  ofstream out(fname.c_str(), ios::app);
-  if (!out) { 
-    cerr << "warning: failed to open " << fname << endl;
-    return false;
-  }
-  if (!in) {
-    in.close();
-    out << "clear ; \
-set terminal png small size 1024,768 ; \n	\
-set output \"" << vname << ".png\" ;		\
-set multiplot ;					\
-set size 1, 1 ;					\
-set origin 0.0,0.0 ;				\
-plot ";
-  } else
-    out << ", ";
-  out << "\"" << vname << ".plot\" using 1:" << col + 1 << " title \"";
-  out << name << "\" with lines";
-  out.close();
-  return true;
+void add_to_plots(plots_t &plots, const string &name, const string &vname,
+		  size_t line, float value) {
+  ostringstream oss;
+  string gnuplot_separator = "_";
+  vector<string> &values = plots[vname][name];
+
+  oss << value;
+  if (values.size() <= line) // resize current array if necessary
+    values.resize(line + 1, gnuplot_separator);
+  // add new value
+  values[line] = oss.str();
 }
 
-int count_words(const string &sentence) {
-  istringstream iss(sentence, istringstream::in);
-  int w = 0;
-  string word;
-
-  while (!iss.eof()) {
-    try {
-      iss >> word;
-      if (word.size() != 0)
-	w++;
-    } catch(std::istringstream::failure& e) { return w; }
-  }
-  return w;
-}
-
-bool append_plot(const string &vname, int line, float value, int &colpos) {
-  string s, fname = vname;
-  fname += ".plot";
-  string fname2 = fname;
-  fname2 += ".tmp";
-  string separator = "\t";
-  string gnuplot_empty_separator = "_";
-  ifstream in(fname.c_str());
-  ofstream out(fname2.c_str());
-  int i;
-
-  if (!out) {
-    cerr << "warning: failed to open " << fname << endl;
-    return false;
-  }
-  i = 0;
-  if (!in) {
-    out << i << separator << value << separator;
-    colpos = 1;
-  } else {
-    i = 0;
-    while (!in.eof()) {
-      getline(in, s);
-      if (colpos < 0)
-	colpos = count_words(s);
-      out << s;
-      if (line == i) {
-	for (int j = count_words(s); j < colpos; ++j) {
-	  if (j == 0)
-	    out << i << separator;
-	  else 
-	    out << gnuplot_empty_separator << separator;
-	}
-	out << value << separator;
-      }
-      if (s.size() > 0)
-	out << endl;
-      i++;
-    }
-    for ( ; i <= line; ++i) {
-      out << i << separator;
-      for (int j = 1; j < colpos; ++j) { 
-	out << gnuplot_empty_separator << separator;
-      }
-      if (i == line) out << value << separator;
-      out << endl;
-    }
-  }
-  out.close();
-  rename(fname2.c_str(), fname.c_str());
-  return true;
-}
-
-bool parse_output_log(const string &fname, const string &name) {
+bool parse_output_log(const string &fname, const string &name, plots_t &plots) {
   cout << "parsing " << fname << endl;
   ifstream in(fname.c_str());
   string s, vname;
   char separator = '=';
-  int line = 0;
+  size_t line = 0;
   float f;
-  int colpos = -1;
   string::size_type itok, stok;
-  bool first = true;
 
   if (!in) {
     cerr << "warning: failed to open " << fname << endl;
@@ -223,15 +144,77 @@ bool parse_output_log(const string &fname, const string &name) {
 	f = numeric_limits<float>::max();
 	iss >> f;
 	itok = s.find(separator);
- 	if (f != numeric_limits<float>::max()) {
-	  append_plot(vname, line, f, colpos);
-	  if (first)
-	    append_plotter(name, vname, colpos);
-	  if (itok == string::npos)
-	    first = false;
-	}
+ 	if (f != numeric_limits<float>::max())
+	  add_to_plots(plots, name, vname, line, f);
       }
     } catch(std::istringstream::failure& e) {}
+  }
+  return true;
+}
+
+bool write_plots(plots_t &plots) {
+  string gnuplot_column_separator = "\t";
+    string gnuplot_config1 = "clear ; \
+set terminal png small size 1024,768 ; \n	\
+set output \"";
+    string gnuplot_config2 = ".png\" ;	\
+set multiplot ;					\
+set size 1, 1 ;					\
+set origin 0.0,0.0 ;				\
+plot ";	    
+  
+  // loop on each plot
+  plots_t::iterator iplots = plots.begin();
+  for ( ; iplots != plots.end(); ++iplots) {
+    // open plot file
+    string plot_fname = iplots->first;
+    plot_fname += ".plot";
+    ofstream out(plot_fname.c_str());
+    if (!out)
+      cerr << "warning: failed to open " << plot_fname << endl;
+    else {
+      // open gnuplot p file
+      string fname = iplots->first;
+      fname += ".p";
+      ofstream outp(fname.c_str());
+      if (!outp)
+	cerr << "warning: failed to open " << fname << endl;
+      else {
+	// initialize .p file
+	outp << gnuplot_config1 << iplots->first << gnuplot_config2;
+	// loop on each run to find maximum number of values and create
+	// the p files describing how to plot the data
+	map<string, vector<string>, less<string> > &runs = iplots->second;
+	map<string, vector<string>, less<string> >::iterator iruns =
+	  runs.begin();
+	size_t max_nvalues = 0;
+	for (int r = 1; iruns != runs.end(); ++iruns, ++r) {
+	  // find maximum number of values
+	  max_nvalues = MAX(max_nvalues, iruns->second.size());
+	  // add instruction to plot this run in the .p file
+	  if (r > 1)
+	    outp << ", ";
+	  outp << "\"" << plot_fname << "\" using 1:" << r << " title \"";
+	  outp << iruns->first << "\" with lines";
+	}
+	// loop on each value (keep looping if until max_nvalues to equalize
+	// the number of values for each run
+	for (size_t i = 0; i < max_nvalues; ++i) {
+	  out << i << gnuplot_column_separator; // separate columns
+	  // loop on each run
+	  for (iruns = runs.begin(); iruns != runs.end(); ++iruns) {
+	    vector<string> &values = iruns->second;
+	    if (i < values.size())
+	      out << values[i] << gnuplot_column_separator;
+	    else
+	      out << "?"  << gnuplot_column_separator;
+	  }
+	  out << endl;
+	}
+      }
+      outp.close();
+    }
+    out.close();
   }
   return true;
 }
@@ -246,7 +229,10 @@ int main(int argc, char **argv) {
   }
 
   string root = argv[1];
-  
+
+  // for each variable, for each logfile, the values of a plot in a vector
+  plots_t plots;
+ 
 #ifdef __BOOST__
     // check root directory
     path rd(root);
@@ -268,11 +254,12 @@ int main(int argc, char **argv) {
 	  if (!is_directory(itr2->status())
 	      && regex_match(itr2->leaf().c_str(), what, output_log)) {
 	    // parse output.log
-	    parse_output_log(itr2->path().string(), itr->leaf());
+	    parse_output_log(itr2->path().string(), itr->leaf(), plots);
 	  }
 	}
       }
     }
+    write_plots(plots);
 #endif 
   return 0;
 }
