06d52cfac79108f26098e334c4640286166fa206
sermanet <sermanet@082c7e19-22b6-4319-94e1-375644db782b>
2010-03-31 06:53:41 +0000

    adding 32bit IPP links

--- trunk/scripts/FindCustom.cmake
+++ trunk/scripts/FindCustom.cmake
@@ -7,8 +7,13 @@
 ################################################################################
 SET(IPP_FOUND FALSE)
 IF (IPP_FOUND)
-  INCLUDE_DIRECTORIES("/opt/intel/ipp/current/em64t/include")
-  LINK_DIRECTORIES("/opt/intel/ipp/current/em64t/sharedlib")
+  INCLUDE_DIRECTORIES("/home/pierre/ipp/include/")
+  LINK_DIRECTORIES("/home/pierre/ipp/sharedlib/")
+  # 64 bit libraries
+  #SET(IPP_LIBRARIES
+  #  ippcoreem64t guide ippiem64t ippcvem64t ippsem64t ippccem64t pthread)
+  # 32 bit libraries
+  SET(IPP_LIBRARIES ippcore guide ippi ippcv ipps ippcc pthread)
   MESSAGE(STATUS "Found Intel IPP")
   SET (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -D__IPP__")
 ENDIF (IPP_FOUND)
