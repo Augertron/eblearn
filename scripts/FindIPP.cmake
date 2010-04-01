51764a6f6abf4f8ed7f681cd1b451375deccb384
sermanet <sermanet@082c7e19-22b6-4319-94e1-375644db782b>
2010-04-01 03:14:48 +0000

    fixing ipp cmake

--- scripts/FindIPP.cmake
+++ scripts/FindIPP.cmake
@@ -6,13 +6,15 @@ FIND_PATH(IPP_INCLUDE_DIR "ipp.h" PATHS
   "$ENV{ProgramFiles}/Intel/IPP/*/ia32/include"
   "/opt/intel/ipp/*/ia32/include"
   "/opt/intel/ipp/*/em64t/include"
-  "/usr/local/intel/ipp/*/ia32/include")
+  "/usr/local/intel/ipp/*/ia32/include"
+  "$ENV{IPPROOT}/include")
 
 FIND_PATH(IPP_LIBRARIES_DIR "libguide.so" PATH
   "$ENV{ProgramFiles}/Intel/IPP/*.*/ia32/sharedlib"
   "/opt/intel/ipp/*/ia32/sharedlib"
   "/opt/intel/ipp/*/em64t/sharedlib"
-  "/usr/local/intel/ipp/*/ia32/sharedlib")
+  "/usr/local/intel/ipp/*/ia32/sharedlib"
+  "$ENV{IPPROOT}/sharedlib")
 
 IF (IPP_INCLUDE_DIR)
   MESSAGE(STATUS "Found Intel IPP include: ${IPP_INCLUDE_DIR}")
