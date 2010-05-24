fabc2ee5072575290ebd64573c6ed813df7704ba
cfarabet <cfarabet@082c7e19-22b6-4319-94e1-375644db782b>
2010-05-24 22:16:14 +0000

    changing qt header paths for compatibility with mac

--- libidxgui/include/window.h
+++ libidxgui/include/window.h
@@ -32,10 +32,10 @@
 #ifndef WINDOW_H_
 #define WINDOW_H_
 
-#include <QtGui/QPixmap>
-#include <QtGui/QWidget>
-#include <QtGui/QtGui>
-#include <QtGui/QResizeEvent>
+#include <QPixmap>
+#include <QWidget>
+#include <QtGui>
+#include <QResizeEvent>
 #include <math.h>
 #include <iostream>
 
--- libidxgui/src/window.cpp
+++ libidxgui/src/window.cpp
@@ -30,7 +30,7 @@
  ***************************************************************************/
 
 #include "moc_window.cxx"
-#include <Qt/qstyle.h>
+#include <qstyle.h>
 #include <math.h>
 
 using namespace std;
