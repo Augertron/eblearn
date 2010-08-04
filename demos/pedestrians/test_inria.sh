3aa014854fc434cc190fc72994107213cc475e64
sermanet <sermanet@082c7e19-22b6-4319-94e1-375644db782b>
2010-08-04 00:08:09 +0000

    fixing bbox output bug in mtdetect

--- demos/pedestrians/test_inria.sh
+++ demos/pedestrians/test_inria.sh
@@ -1,5 +1,16 @@
 #!/bin/sh
 
+vgdir=~/visiongrader/
+inria=/data/pedestrians/INRIAPerson/Test/annotations/
+#inria=${hostname}adata/pedestrians/test/inria/
+
 # generate curves for inria dataset using visiongrader project
 # $1 should be the bounding box output file
-python ~/visiongrader/main.py --input $1 --input_parser eblearn --groundtruth ${hostname}adata/pedestrians/test/inria/ --groundtruth_parser INRIA --comparator overlap50percent --det
+python $vgdir/main.py --input $1 --input_parser eblearn \
+    --groundtruth $inria  --groundtruth_parser inria \
+    --comparator overlap50percent --det --saving-file inria.curve \
+    --confidence_min -5 --confidence_max 5
+
+# plot
+python $vgdir/plotpickle.py --main_curve inria.curve \
+    --xmin 0.003 --xmax 102 --ymin .03 --ymax 1.1 *.curve
