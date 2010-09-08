7f301827ee00c65d18d58becafa5a78d85a80845
sermanet <sermanet@082c7e19-22b6-4319-94e1-375644db782b>
2010-09-08 19:11:02 +0000

    fixing last detections in mtdetect

--- demos/pedestrians/test_inria.sh
+++ demos/pedestrians/test_inria.sh
@@ -1,22 +1,29 @@
 #!/bin/sh
 
 vgdir=~/visiongrader/
-inria=/data/pedestrians/INRIAPerson/Test/annotations/
+inria=/data/pedestrians/inria/INRIAPerson/Test/annotations/
 #inria=${hostname}adata/pedestrians/test/inria/
 #inria=/data/pedestrians/inria_caltech/annotations/set01/V000.vbb
 #gtparser=caltech #inria
 gtparser=inria
 
+# # generate curves for inria dataset using visiongrader project
+# # $1 should be the bounding box output file
+# python $vgdir/main.py --input $1 --input_parser eblearn \
+#     --groundtruth $inria  --groundtruth_parser $gtparser \
+#     --comparator overlap50percent --det --saving-file $1.curve \
+# #    --confidence_min -5 # --confidence_max 5
+
 # generate curves for inria dataset using visiongrader project
 # $1 should be the bounding box output file
 python $vgdir/main.py --input $1 --input_parser eblearn \
     --groundtruth $inria  --groundtruth_parser $gtparser \
-    --comparator overlap50percent --det --saving-file inria.curve \
-#    --confidence_min -5 # --confidence_max 5
+    --comparator overlap50percent --det --saving-file $1.curve \
+    --confidence_min -5 # --confidence_max 5
 
 # plot
-python $vgdir/plotpickle.py --main_curve inria.curve \
-    --xmin 0.003 --xmax 102 --ymin .03 --ymax 1.1 *.curve \
+python $vgdir/plotpickle.py --main_curve $1.curve \
+    --xmin 0.003 --xmax 102 --ymin .03 --ymax 1.1 bbox/*.curve \
     --xlegend "False positives per image" --ylegend "Miss rate"
 
 # show db
